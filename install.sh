e871db2db4567c35e1f73ec76f25aa8f5ecd2611
NSTFREENET-ID <68092423+nstfreenet@users.noreply.github.com>
2020-10-21 05:43:48 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 # Original script by fornesia, rzengineer and fawzya 
-# Mod by Rizwan Arif Firmansyah
+# Mod By Zuan We Lose
 # 
 # ==================================================
 
@@ -90,7 +90,7 @@ echo 'echo -e "██      ██   ██ ██  ██ 
 echo 'echo -e "██      ██   ██ ██      ██ ██ ███████ ██    "' >> .bashrc
 echo 'echo -e "                                      The Big Kadall FamilyGroup™" | lolcat' >> .bashrc
 echo 'echo -e "welcome to the server $HOSTNAME" | lolcat' >> .bashrc
-echo 'echo -e "Script mod by Rizwan Arif Firmansyah" | lolcat' >> .bashrc
+echo 'echo -e "Script mod Zuan We Lose" | lolcat' >> .bashrc
 echo 'echo -e "Type menu to display a list of commands" | lolcat' >> .bashrc
 echo 'echo -e ""' >> .bashrc
 
@@ -98,23 +98,23 @@ echo 'echo -e ""' >> .bashrc
 cd
 rm /etc/nginx/sites-enabled/default
 rm /etc/nginx/sites-available/default
-wget -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/acillsadank/install/master/nginx.conf"
+wget -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/nstfreenet/debian/main/nginx.conf"
 mkdir -p /home/vps/public_html
-echo "<pre>Setup by KadallFamily</pre>" > /home/vps/public_html/index.html
-wget -O /etc/nginx/conf.d/vps.conf "https://raw.githubusercontent.com/acillsadank/install/master/vps.conf"
+echo "<pre>Setup by JSTFREENET</pre>" > /home/vps/public_html/index.html
+wget -O /etc/nginx/conf.d/vps.conf "https://raw.githubusercontent.com/nstfreenet/debian/main/vps.conf"
 
 # install openvpn
 apt-get -y install openvpn easy-rsa openssl
 cp -r /usr/share/easy-rsa/ /etc/openvpn
 mkdir /etc/openvpn/easy-rsa/keys
 sed -i 's|export KEY_COUNTRY="US"|export KEY_COUNTRY="ID"|' /etc/openvpn/easy-rsa/vars
-sed -i 's|export KEY_PROVINCE="CA"|export KEY_PROVINCE="JABAR"|' /etc/openvpn/easy-rsa/vars
-sed -i 's|export KEY_CITY="SanFrancisco"|export KEY_CITY="PURWAKARTA"|' /etc/openvpn/easy-rsa/vars
-sed -i 's|export KEY_ORG="Fort-Funston"|export KEY_ORG="RizwanArifFirmansyah"|' /etc/openvpn/easy-rsa/vars
-sed -i 's|export KEY_EMAIL="me@myhost.mydomain"|export KEY_EMAIL="acill.sadank@gmail.com"|' /etc/openvpn/easy-rsa/vars
-sed -i 's|export KEY_OU="MyOrganizationalUnit"|export KEY_OU="RizwanArifFirmansyah"|' /etc/openvpn/easy-rsa/vars
-sed -i 's|export KEY_NAME="EasyRSA"|export KEY_NAME="RizwanArifFirmansyah"|' /etc/openvpn/easy-rsa/vars
-sed -i 's|export KEY_OU=changeme|export KEY_OU="RizwanArifFirmansyah" |' /etc/openvpn/easy-rsa/vars
+sed -i 's|export KEY_PROVINCE="CA"|export KEY_PROVINCE="JATENG"|' /etc/openvpn/easy-rsa/vars
+sed -i 's|export KEY_CITY="SanFrancisco"|export KEY_CITY="GROBOGAN"|' /etc/openvpn/easy-rsa/vars
+sed -i 's|export KEY_ORG="Fort-Funston"|export KEY_ORG="ZuanWeLose"|' /etc/openvpn/easy-rsa/vars
+sed -i 's|export KEY_EMAIL="me@myhost.mydomain"|export KEY_EMAIL="Zuan@gmail.com"|' /etc/openvpn/easy-rsa/vars
+sed -i 's|export KEY_OU="MyOrganizationalUnit"|export KEY_OU="ZuanWeLose"|' /etc/openvpn/easy-rsa/vars
+sed -i 's|export KEY_NAME="EasyRSA"|export KEY_NAME="ZuanWeLose"|' /etc/openvpn/easy-rsa/vars
+sed -i 's|export KEY_OU=changeme|export KEY_OU="ZuanWeLose" |' /etc/openvpn/easy-rsa/vars
 
 # Create Diffie-Helman Pem
 openssl dhparam -out /etc/openvpn/dh2048.pem 2048
@@ -144,27 +144,27 @@ chmod +x /etc/openvpn/ca.crt
 
 # server settings
 cd /etc/openvpn/
-wget -O /etc/openvpn/server.conf "https://raw.githubusercontent.com/acillsadank/install/master/server.conf"
+wget -O /etc/openvpn/server.conf "https://raw.githubusercontent.com/nstfreenet/debian/main/server.conf"
 systemctl start openvpn@server
 sysctl -w net.ipv4.ip_forward=1
 sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g' /etc/sysctl.conf
 iptables -t nat -I POSTROUTING -s 192.168.100.0/24 -o eth0 -j MASQUERADE
 iptables -t nat -I POSTROUTING -o eth0 -j MASQUERADE
 iptables-save > /etc/iptables.up.rules
-wget -O /etc/network/if-up.d/iptables "https://raw.githubusercontent.com/acillsadank/install/master/iptables"
+wget -O /etc/network/if-up.d/iptables "https://raw.githubusercontent.com/nstfreenet/debian/main/iptables"
 chmod +x /etc/network/if-up.d/iptables
 sed -i 's|LimitNPROC|#LimitNPROC|g' /lib/systemd/system/openvpn@.service
 systemctl daemon-reload
 /etc/init.d/openvpn restart
 
 # openvpn config
-wget -O /etc/openvpn/client.ovpn "https://raw.githubusercontent.com/acillsadank/install/master/client.conf"
+wget -O /etc/openvpn/client.ovpn "https://raw.githubusercontent.com/nstfreenet/debian/main/client.conf"
 sed -i $MYIP2 /etc/openvpn/client.ovpn;
 echo '<ca>' >> /etc/openvpn/client.ovpn
 cat /etc/openvpn/ca.crt >> /etc/openvpn/client.ovpn
 echo '</ca>' >> /etc/openvpn/client.ovpn
 cp client.ovpn /home/vps/public_html/
-wget -O /etc/openvpn/openvpnssl.ovpn "https://raw.githubusercontent.com/acillsadank/install/master/openvpnssl.conf"
+wget -O /etc/openvpn/openvpnssl.ovpn "https://raw.githubusercontent.com/nstfreenet/debian/main/openvpnssl.conf"
 echo '<ca>' >> /etc/openvpn/openvpnssl.ovpn
 cat /etc/openvpn/ca.crt >> /etc/openvpn/openvpnssl.ovpn
 echo '</ca>' >> /etc/openvpn/openvpnssl.ovpn
@@ -172,9 +172,9 @@ cp openvpnssl.ovpn /home/vps/public_html/
 
 # install badvpn
 cd
-wget -O /usr/bin/badvpn-udpgw "https://raw.githubusercontent.com/acillsadank/install/master/badvpn-udpgw"
+wget -O /usr/bin/badvpn-udpgw "https://raw.githubusercontent.com/nstfreenet/debian/main/badvpn-udpgw"
 if [ "$OS" == "x86_64" ]; then
-  wget -O /usr/bin/badvpn-udpgw "https://raw.githubusercontent.com/acillsadank/install/master/badvpn-udpgw64"
+  wget -O /usr/bin/badvpn-udpgw "https://raw.githubusercontent.com/nstfreenet/debian/main/badvpn-udpgw64"
 fi
 sed -i '$ i\screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7200' /etc/rc.local
 chmod +x /usr/bin/badvpn-udpgw
@@ -194,7 +194,7 @@ echo "/usr/sbin/nologin" >> /etc/shells
 
 # install squid
 apt-get -y install squid
-wget -O /etc/squid/squid.conf "https://raw.githubusercontent.com/acillsadank/install/master/squid3.conf"
+wget -O /etc/squid/squid.conf "https://raw.githubusercontent.com/nstfreenet/debian/main/edit-squid3.sh"
 sed -i $MYIP2 /etc/squid/squid.conf;
 
 # install webmin
@@ -214,6 +214,18 @@ socket = r:TCP_NODELAY=1
 accept = 443
 connect = 127.0.0.1:143
 
+[dropbear]
+accept = 444
+connect = 127.0.0.1:143
+
+[dropbear]
+accept = 445
+connect = 127.0.0.1:143
+
+[dropbear]
+accept = 446
+connect = 127.0.0.1:143
+
 [openvpn]
 accept = 990
 connect = 127.0.0.1:1194
@@ -229,7 +241,7 @@ cat key.pem cert.pem >> /etc/stunnel/stunnel.pem
 # configure stunnel
 sed -i 's/ENABLED=0/ENABLED=1/g' /etc/default/stunnel4
 cd /etc/stunnel/
-wget -O /etc/stunnel/ssl.conf "https://raw.githubusercontent.com/acillsadank/install/master/ssl.conf"
+wget -O /etc/stunnel/ssl.conf "https://raw.githubusercontent.com/nstfreenet/debian/main/ssl.conf"
 sed -i $MYIP2 /etc/stunnel/ssl.conf;
 cp ssl.conf /home/vps/public_html/
 cd
@@ -244,14 +256,14 @@ apt-get -y install fail2ban
 # install ddos deflate
 cd
 apt-get -y install dnsutils dsniff
-wget https://raw.githubusercontent.com/acillsadank/install/master/ddos-deflate-master.zip
+wget https://raw.githubusercontent.com/nstfreenet/debian/main/ddos-deflate-master.zip
 unzip ddos-deflate-master.zip
 cd ddos-deflate-master
 ./install.sh
 rm -rf /root/ddos-deflate-master.zip
 
 # banner /etc/bnr
-wget -O /etc/banner.txt "https://raw.githubusercontent.com/acillsadank/install/master/banner.txt"
+wget -O /etc/banner.txt "https://raw.githubusercontent.com/nstfreenet/debian/main/banner.txt"
 sed -i 's@#Banner@Banner@g' /etc/ssh/sshd_config
 sed -i 's@DROPBEAR_BANNER=""@DROPBEAR_BANNER="/etc/banner.txt"@g' /etc/default/dropbear
 
@@ -261,23 +273,23 @@ apt-get install -y libxml-parser-perl
 
 # download script
 cd /usr/bin
-wget -O menu "https://raw.githubusercontent.com/acillsadank/install/master/menu.sh"
-wget -O edit "https://raw.githubusercontent.com/acillsadank/install/master/edit-ports.sh"
-wget -O edit-dropbear "https://raw.githubusercontent.com/acillsadank/install/master/edit-dropbear.sh"
-wget -O edit-openssh "https://raw.githubusercontent.com/acillsadank/install/master/edit-openssh.sh"
-wget -O edit-openvpn "https://raw.githubusercontent.com/acillsadank/install/master/edit-openvpn.sh"
-wget -O edit-squid3 "https://raw.githubusercontent.com/acillsadank/install/master/edit-squid3.sh"
-wget -O edit-stunnel4 "https://raw.githubusercontent.com/acillsadank/install/master/edit-stunnel4.sh"
-wget -O show-ports "https://raw.githubusercontent.com/acillsadank/install/master/show-ports.sh"
-wget -O usernew "https://raw.githubusercontent.com/acillsadank/install/master/usernew.sh"
-wget -O trial "https://raw.githubusercontent.com/acillsadank/install/master/trial.sh"
-wget -O delete "https://raw.githubusercontent.com/acillsadank/install/master/delete.sh"
-wget -O check "https://raw.githubusercontent.com/acillsadank/install/master/user-login.sh"
-wget -O member "https://raw.githubusercontent.com/acillsadank/install/master/user-list.sh"
-wget -O restart "https://raw.githubusercontent.com/acillsadank/install/master/restart.sh"
-wget -O speedtest "https://raw.githubusercontent.com/acillsadank/install/master/speedtest_cli.py"
-wget -O info "https://raw.githubusercontent.com/acillsadank/install/master/info.sh"
-wget -O about "https://raw.githubusercontent.com/acillsadank/install/master/about.sh"
+wget -O menu "https://raw.githubusercontent.com/nstfreenet/debian/main/menu.sh"
+wget -O edit "https://raw.githubusercontent.com/nstfreenet/debian/main/edit-ports.sh"
+wget -O edit-dropbear "https://raw.githubusercontent.com/nstfreenet/debian/main/edit-dropbear.sh"
+wget -O edit-openssh "https://raw.githubusercontent.com/nstfreenet/debian/main/edit-openssh.sh"
+wget -O edit-openvpn "https://raw.githubusercontent.com/nstfreenet/debian/main/edit-openvpn.sh"
+wget -O edit-squid3 "https://raw.githubusercontent.com/nstfreenet/debian/main/edit-squid3.sh"
+wget -O edit-stunnel4 "https://raw.githubusercontent.com/nstfreenet/debian/main/edit-stunnel4.sh"
+wget -O show-ports "https://raw.githubusercontent.com/nstfreenet/debian/main/show-ports.sh"
+wget -O usernew "https://raw.githubusercontent.com/nstfreenet/debian/main/usernew.sh"
+wget -O trial "https://raw.githubusercontent.com/nstfreenet/debian/main/trial.sh"
+wget -O delete "https://raw.githubusercontent.com/nstfreenet/debian/main/delete.sh"
+wget -O check "https://raw.githubusercontent.com/nstfreenet/debian/main/user-login.sh"
+wget -O member "https://raw.githubusercontent.com/nstfreenet/debian/main/user-list.sh"
+wget -O restart "https://raw.githubusercontent.com/nstfreenet/debian/main/restart.sh"
+wget -O speedtest "https://raw.githubusercontent.com/nstfreenet/debian/main/speedtest_cli.py"
+wget -O info "https://raw.githubusercontent.com/nstfreenet/debian/main/info.sh"
+wget -O about "https://raw.githubusercontent.com/nstfreenet/debian/main/about.sh"
 
 chmod +x menu
 chmod +x edit
@@ -340,7 +352,7 @@ echo "Service"  | tee -a log-install.txt
 echo "-------"  | tee -a log-install.txt
 echo "OpenSSH  : 22"  | tee -a log-install.txt
 echo "Dropbear : 143, 456, 999"  | tee -a log-install.txt
-echo "SSL      : 443"  | tee -a log-install.txt
+echo "SSL      : 443,444,445,446"  | tee -a log-install.txt
 echo "OpenVPNSSL : 990"  | tee -a log-install.txt
 echo "Squid3   : 80, 8080 (limit to IP SSH)"  | tee -a log-install.txt
 echo "SSL      : http://$MYIP:81/ssl.conf"  | tee -a log-install.txt
@@ -370,7 +382,7 @@ echo "Timezone : Asia/Jakarta (GMT +7)"  | tee -a log-install.txt
 echo "IPv6     : [off]"  | tee -a log-install.txt
 echo ""  | tee -a log-install.txt
 echo "Original Script by Fornesia, Rzengineer & Fawzya"  | tee -a log-install.txt
-echo "Modified by Rizwan Arif Firmansyah"  | tee -a log-install.txt
+echo "Modified by Zuan We Lose"  | tee -a log-install.txt
 echo ""  | tee -a log-install.txt
 echo "Installation Log --> /root/log-install.txt"  | tee -a log-install.txt
 echo ""  | tee -a log-install.txt
