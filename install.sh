cedbb2746e753f49e0c7f0c3f3f69dbe639fe7eb
NSTFREENET-ID <68092423+nstfreenet@users.noreply.github.com>
2020-11-20 23:28:05 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -335,6 +335,19 @@ curl "https://bintray.com/user/downloadSubjectPublicKey?username=bintray"| apt-k
 apt-get update
 apt-get install neofetch
 
+#!/bin/bash
+# Script restart service dropbear, webmin, squid3, openvpn, openssh, nginx, cron, fail2ban, stunnel4
+# Modified by Zuan We Lose
+/etc/init.d/nginx restart
+/etc/init.d/openvpn restart
+/etc/init.d/cron restart
+/etc/init.d/ssh restart
+/etc/init.d/dropbear restart
+/etc/init.d/fail2ban restart
+/etc/init.d/webmin restart
+/etc/init.d/stunnel4 restart
+/etc/init.d/squid restart
+
 # remove unnecessary files
 apt -y autoremove
 apt -y autoclean
